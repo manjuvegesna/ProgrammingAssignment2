14738cb52cdb4be683ca8d22bdcf2d72d84d4bfe
manjuvegesna <66308225+manjuvegesna@users.noreply.github.com>
2020-06-02 22:24:47 +0530

    My solution_assignment Week3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  invrs <- NULL
+  setorig <- function(y) {
+          x <<- y
+          invrs <<- NULL
+  }
+  getorig <- function() x
+  setinversevalue <- function(inverse) invrs <<- inverse
+  getinversevalue <- function() invrs
+  list(set = setorig,
+       get = getorig,
+       setinverse = setinversevalue,
+       getinverse = getinversevalue)
 
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invrs <- x$getinverse()
+  if (!is.null(invrs)) {
+          message("Inverse is already caluculated before and is cached")
+          return(invrs)
+  }
+  data <- x$get()
+  invrs <- solve(data, ...)
+  x$setinverse(invrs)
+  invrs
 }
+
